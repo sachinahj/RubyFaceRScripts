046f90193ff9c843699deb93871caf1e0b9ae600
Sachin Ahuja <sachinahj@gmail.com>
2014-10-01 18:17:43 -0500

    commented test.rb

--- test.rb
+++ test.rb
@@ -4,10 +4,16 @@ require_relative "./scripts/recognize_subject.rb"
 require_relative "./scripts/galleries.rb"
 
 
-# FaceR::EnrollSubject.run("./C7/Jwarren.JPG", "MKS", "Jeremy")
-# FaceR::Galleries.view_galleries
-# FaceR::Galleries.view_subjects("MKS")
+# FaceR::EnrollSubject.run(image_path, gallery_id, subject_id)
+FaceR::EnrollSubject.run("./C7/Jwarren.JPG", "MKS", "Jeremy")
 
+# FaceR::Galleries.view_galleries
+FaceR::Galleries.view_galleries
+
+# FaceR::Galleries.subjects(subject_id)
+FaceR::Galleries.view_subjects("MKS")
+
+# FaceR::RecognizeSubject.run(image_path, gallery_id)
 FaceR::RecognizeSubject.run("./TestPhotos/sachin.jpeg", "MKS")
 
 
